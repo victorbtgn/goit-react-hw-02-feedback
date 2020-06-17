501ef48226a8e36849ded7eed94a5805c73e9a8c
Victor Batigin <bixs85@gmail.com>
2020-06-17 21:56:07 +0300

    Updates

---
+++ precache-manifest.0c93b1bbf0ca81553fcd1c0b881edc39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38cac656538d87fea971987e14977736",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "c6cbdb3c7a4318d23098",
+    "url": "/goit-react-hw-02-feedback/static/css/main.133edf3e.chunk.css"
+  },
+  {
+    "revision": "9b2bf4c9921ced580578",
+    "url": "/goit-react-hw-02-feedback/static/js/2.d6ce7a72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.d6ce7a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6cbdb3c7a4318d23098",
+    "url": "/goit-react-hw-02-feedback/static/js/main.aab9f7ab.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
